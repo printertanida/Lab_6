2f87c116f0c129b0c082bbd3e612cc6078b25cca
Tanida <printer.rung@gmail.com>
2024-12-03 18:31:03 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int number, evenCount = 0, oddCount = 0;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    while (cin >> number && number != 0) {
+        if (number % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+        cout << "Enter an integer: ";
+    }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+
     return 0;
 }
